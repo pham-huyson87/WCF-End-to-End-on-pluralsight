6f6aa83e4b00d16cf9e43d83c32668eeab72a17e
pham-huyson87 <pham.huyson87@hotmail.com>
2017-12-07 22:16:01 -0500

    Service Contract
    Add of IGeoService ServiceContract

---
+++ GeoLib.Contracts/IGeoService.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace GeoLib.Contracts
+{
+    [ServiceContract]
+    public  interface IGeoService
+    {
+        [OperationContract]                                         // ServiceContract is opt-in
+        ZipCodeData GetZipInfo(string zip);
+
+        [OperationContract]                                         // The name of operation
+        IEnumerable<string> GetStates(bool primaryOnly);            // by defaut is the name of the method.
+
+        [OperationContract(Name = "GetZipsByState")]
+        IEnumerable<ZipCodeData> GetZips(string state);             // We have conflicted names, 
+                                                                    // if we don't explicit the operation names
+        [OperationContract(Name = "GetZipsForRange")]
+        IEnumerable<ZipCodeData> GetZips(string zip, int range);
+    }
+}
