5b02c52db530be719eadff92a54ac6731f38d9d5
pham-huyson87 <pham.huyson87@hotmail.com>
2017-12-07 21:59:12 -0500

    Data Contract
    Creation of ZipCodeData Data Contract

---
+++ GeoLib.Contracts/ZipCodeData.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace GeoLib.Contracts
+{
+
+    [DataContract]                                  // Opt-in serializer :  explicit it
+    public class ZipCodeData                        // Data postfix :       WCF standard
+    {
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+        [DataMember]
+        public string ZipCode { get; set; }
+    }
+}
