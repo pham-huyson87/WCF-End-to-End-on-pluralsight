ffcdc868354cf43e7d9e00d813037eacdde00eb1
pham-huyson87 <pham.huyson87@hotmail.com>
2018-01-20 17:05:07 -0500

    Service hosting
    Create a console application that hosts the GeoManager service.
    The application open a connection to listen the requests that will be sent.

---
+++ GeoLib.ConsoleHost/Program.cs
@@ -0,0 +1,20 @@
+﻿using GeoLib.Services;
+using System;
+using System.ServiceModel;
+
+namespace GeoLib.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+            hostGeoManager.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to exit.");
+            Console.ReadLine();
+
+            hostGeoManager.Close();
+        }
+    }
+}
--- GeoLib.Services/GeoManager.cs
+++ GeoLib.Services/GeoManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GeoLib.Contracts;
 using GeoLib.Data;
 
