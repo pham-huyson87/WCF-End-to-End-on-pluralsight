ab27614a8169b073ff1c4720b9294a3f1326dc3e
pham-huyson87 <pham.huyson87@hotmail.com>
2018-01-20 17:57:04 -0500

    Creation of Custom Service Host Factory
    The Custom Service Host Factory does the same thing like the default one.

---
+++ GeoLib.WebHost/CustomHostFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace GeoLib.WebHost
+{
+    public class CustomHostFactory : ServiceHostFactory
+    {
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            // The default host factory does the same thing, but the can custom it now
+            ServiceHost host = new ServiceHost(serviceType, baseAddresses);
+            return host;
+        }
+    }
+}
